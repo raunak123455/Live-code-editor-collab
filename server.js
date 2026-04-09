8102f33b1a0093e4926c4aad097fed6378170fa5
raunak123455 <raunakrana101@gmail.com>
2026-04-09 14:24:20 +0530

    Add Render deployment configuration and CORS setup

--- server.js
+++ server.js
@@ -1,70 +1,84 @@
-const express = require('express');
-const http = require('http');
-const path = require('path');
-const { Server } = require('socket.io');
-const ACTIONS = require('./src/actions');
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const { Server } = require("socket.io");
+const ACTIONS = require("./src/actions");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+
+// CORS configuration for production
+const allowedOrigins = process.env.ALLOWED_ORIGINS
+  ? process.env.ALLOWED_ORIGINS.split(",")
+  : ["http://localhost:3000"];
+
+const io = new Server(server, {
+  cors: {
+    origin: allowedOrigins,
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
 
 const userSocketMap = {};
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, "build")));
 
 function getAllConnectedClients(roomId) {
-    return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map((socketId) => ({
-        socketId,
-        username: userSocketMap[socketId],
-    }));
+  return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
+    (socketId) => ({
+      socketId,
+      username: userSocketMap[socketId],
+    }),
+  );
 }
 
-io.on('connection', (socket) => {
-    console.log('socket connected:', socket.id);
+io.on("connection", (socket) => {
+  console.log("socket connected:", socket.id);
 
-    socket.on(ACTIONS.JOIN, ({ roomId, username }) => {
-        userSocketMap[socket.id] = username;
-        socket.join(roomId);
-        const clients = getAllConnectedClients(roomId);
-        clients.forEach(({ socketId }) => {
-            io.to(socketId).emit(ACTIONS.JOINED, {
-                clients,
-                username,
-                socketId: socket.id,
-            });
-        });
+  socket.on(ACTIONS.JOIN, ({ roomId, username }) => {
+    userSocketMap[socket.id] = username;
+    socket.join(roomId);
+    const clients = getAllConnectedClients(roomId);
+    clients.forEach(({ socketId }) => {
+      io.to(socketId).emit(ACTIONS.JOINED, {
+        clients,
+        username,
+        socketId: socket.id,
+      });
     });
+  });
 
-    socket.on(ACTIONS.CODE_CHANGE, ({ roomId, code }) => {
-        socket.in(roomId).emit(ACTIONS.CODE_CHANGE, { code });
-    });
+  socket.on(ACTIONS.CODE_CHANGE, ({ roomId, code }) => {
+    socket.in(roomId).emit(ACTIONS.CODE_CHANGE, { code });
+  });
 
-    socket.on(ACTIONS.SYNC_CODE, ({ socketId, code }) => {
-        io.to(socketId).emit(ACTIONS.CODE_CHANGE, { code });
-    });
+  socket.on(ACTIONS.SYNC_CODE, ({ socketId, code }) => {
+    io.to(socketId).emit(ACTIONS.CODE_CHANGE, { code });
+  });
 
-    socket.on(ACTIONS.TYPING, ({ roomId, username, isTyping }) => {
-        socket.in(roomId).emit(ACTIONS.TYPING, { username, isTyping });
-    });
+  socket.on(ACTIONS.TYPING, ({ roomId, username, isTyping }) => {
+    socket.in(roomId).emit(ACTIONS.TYPING, { username, isTyping });
+  });
 
-    socket.on(ACTIONS.CURSOR_CHANGE, ({ roomId, username, cursor }) => {
-        socket.in(roomId).emit(ACTIONS.CURSOR_CHANGE, { username, cursor });
-    });
+  socket.on(ACTIONS.CURSOR_CHANGE, ({ roomId, username, cursor }) => {
+    socket.in(roomId).emit(ACTIONS.CURSOR_CHANGE, { username, cursor });
+  });
 
-    socket.on('disconnecting', () => {
-        const rooms = [...socket.rooms];
-        rooms.forEach((roomId) => {
-            socket.in(roomId).emit(ACTIONS.DISCONNECTED, {
-                socketId: socket.id,
-                username: userSocketMap[socket.id],
-            });
-        });
-        delete userSocketMap[socket.id];
+  socket.on("disconnecting", () => {
+    const rooms = [...socket.rooms];
+    rooms.forEach((roomId) => {
+      socket.in(roomId).emit(ACTIONS.DISCONNECTED, {
+        socketId: socket.id,
+        username: userSocketMap[socket.id],
+      });
     });
+    delete userSocketMap[socket.id];
+  });
 });
 
-app.get('*', (_req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("*", (_req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
